a41339d8336ca9dc9b21743aaf812f104112c5df
Omar Saqr <saqrware@gmail.com>
2019-05-26 03:32:04 +0200

    add recursion

--- 1. Factorial/src/main/scala/Main.scala
+++ 1. Factorial/src/main/scala/Main.scala
@@ -4,13 +4,20 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     println(factorial(5))
+    println(factorialRecursive(5))
   }
 
-  //Factorial function
+  // Factorial function
   def factorial(n: Int) = {
     var result = 1
     for (i <- 1 to n)
       result = result * i
     result
+  }
+
+  // With recursion
+  def factorialRecursive(n: Int): Int = {
+    if (n <= 0) 1
+    else n * factorialRecursive(n - 1)
   }
 }
