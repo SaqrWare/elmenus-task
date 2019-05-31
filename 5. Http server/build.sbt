ad0698544f15315dfad9dcb4dfd848888a53b99f
Omar Saqr <saqrware@gmail.com>
2019-05-31 05:35:46 +0200

    filter closed

--- 5. Http server/build.sbt
+++ 5. Http server/build.sbt
@@ -1,16 +1,9 @@
-name := "HttpServer"
+name := "HelloWorld"
 
 version := "0.1"
 
 scalaVersion := "2.12.8"
 
-
 libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.1.8"
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.19"
-
-val circeVersion = "0.7.0"
-libraryDependencies ++= Seq(
-  "io.circe"  %% "circe-core"     % circeVersion,
-  "io.circe"  %% "circe-generic"  % circeVersion,
-  "io.circe"  %% "circe-parser"   % circeVersion
-)
+libraryDependencies += "net.liftweb" %% "lift-json" % "3.3.0"
--- 5. Http server/src/main/scala/Main.scala
+++ 5. Http server/src/main/scala/Main.scala
@@ -1,44 +1,45 @@
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.HttpEntity
-import akka.http.scaladsl.model.ContentTypes
 import akka.http.scaladsl.Http
-import scala.io.StdIn
-import scala.concurrent.Future
-import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.PathMatcher
-import akka.http.scaladsl.model.StatusCodes
-import scala.io.Source
-import io.circe.parser
-import io.circe.generic.semiauto.deriveDecoder
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import net.liftweb.json.Serialization.write
+import net.liftweb.json._
 
-//import spray.json.DefaultJsonProtocol._
-//import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import scala.io.Source
+
+case class RestaurantData(
+                           enName: String,
+                           arName: String,
+                           state: String,
+                           routingMethod: Any,
+                           logo: String,
+                           coverPhoto: String,
+                           enDescription: String,
+                           arDescription: String,
+                           shortNumber: String,
+                           facebookLink: String,
+                           twitterLink: String,
+                           youtubeLink: String,
+                           website: String,
+                           onlinePayment: Boolean,
+                           client: Boolean,
+                           pendingInfo: Boolean,
+                           pendingMenu: Boolean,
+                           closed: Boolean
+                         )
+
+case class Restaurant(
+                       uuid: String,
+                       data: RestaurantData
+                     )
 
 object Main {
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
-    case class Restaurant(enName: String,
-                          arName: String,
-                          state: String,
-                          routingMethod: Any,
-                          logo: String,
-                          coverPhoto: String,
-                          enDescription: String,
-                          arDescription: String,
-                          shortNumber: String,
-                          facebookLink: String,
-                          twitterLink: String,
-                          youtubeLink: String,
-                          website: String,
-                          onlinePayment: Boolean,
-                          client: Boolean,
-                          pendingInfo: Boolean,
-                          pendingMenu: Boolean,
-                          closed: Boolean)
+
 
     val route =
       path("api" / "restaurant") {
@@ -51,8 +52,14 @@ object Main {
               }
               else {
                 var isClosed = if (closed == Option("1")) true else false
-                println(isClosed)
-                complete(HttpEntity(ContentTypes.`application/json`, restaurants))
+                val restaurantsList = decodeRestaurants(restaurants)
+                if (!isClosed) {
+                  val restaurantsFiltered = restaurantsList.filter(x => x.data.closed == false)
+                  val restaurantsFilteredText = encodeRestaurants(restaurantsFiltered.toList)
+                  complete(HttpEntity(ContentTypes.`application/json`, restaurantsFilteredText))
+                } else {
+                  complete(HttpEntity(ContentTypes.`application/json`, restaurants))
+                }
               }
             }
           }
@@ -70,7 +77,16 @@ object Main {
 
   def getRestaurants(): String = readFile("data.json")
 
-  def decodeRestaurants() = {
+  def decodeRestaurants(restaurantsText: String) = {
+    implicit val formats = DefaultFormats
+    val jValue = parse(restaurantsText)
+    val result = jValue.extract[List[Restaurant]]
+    result.toArray
+  }
 
+  def encodeRestaurants(restaurants: List[Restaurant]) = {
+    implicit val formats = DefaultFormats
+    val jsonString = write(restaurants)
+    jsonString
   }
 }
